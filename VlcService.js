abb000aa4d04372086fc2bdab735b660cf0d722c
unknown <apetersen@LT0191.skyline.gb>
2014-02-12 23:51:05 -0600

    initial commit

---
+++ VlcService.js
@@ -0,0 +1,46 @@
+var HttpService = require("droopy-http"),
+	convertXmlToJs = require('xml2js').parseString,
+	q = require("q");
+	models = require("../../models");
+
+var VlcService = function(url) {
+	this.baseUrl = url + "/requests/status.xml";
+};
+
+VlcService.prototype = new HttpService();
+
+VlcService.prototype._statusRequest = function(command, value) {
+	var deferred = q.defer();
+	var url = this.baseUrl;
+	if (command) {
+		url += ("?command=" + command);
+		if (value) {
+			url += ("&val=" + value);
+		}
+	}
+	this.get(url, {}, true).then(function(xml) {
+		var status = new models.VlcStatus(xml);
+		deferred.resolve(status);
+	}).fail(function(){
+		deferred.reject(arguments);
+	});
+	return deferred.promise;
+};
+
+VlcService.prototype.status = function() {
+	return this._statusRequest();
+};
+
+VlcService.prototype.seek = function(time) {
+	return this._statusRequest("seek", time);
+};
+
+VlcService.prototype.volume = function(volume) {
+	var v = ((volume * 512) / 200).toFixed(0);
+	return this._statusRequest("volume", v);
+};
+VlcService.prototype.togglePause = function() {
+	return this._statusRequest("pl_pause");
+};
+
+module.exports = VlcService;
